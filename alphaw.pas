bbc5a828e74d560b6be8bcde2a54cef0c1fe4886
hippy2094 <hippy2094@gmail.com>
2013-01-26 20:05:37 +0000

    Added Google Sitemaps compatible XML output

--- alphaw.pas
+++ alphaw.pas
@@ -1,7 +1,7 @@
 unit alphaw;
 (*
 Simple Sitemap Creator
-Copyright (C) 2010-2012 Matthew Hipkin <http://www.matthewhipkin.co.uk>
+Copyright (C) 2010 Matthew Hipkin <http://www.matthewhipkin.co.uk>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
